9a2330ae9a9117f878f9a42ffc5b9d81f404673c
KhageshorGiri <girikhageshor2432@gmail.com>
2024-10-10 20:22:46 +0545

    Added Endpoint for CRUD Operation

--- TygrLabsAssignmentAPI/src/TygrLabs.API/Controllers/UsersController.cs
+++ TygrLabsAssignmentAPI/src/TygrLabs.API/Controllers/UsersController.cs
@@ -6,4 +6,34 @@ namespace TygrLabs.API.Controllers;
 [ApiController]
 public class UsersController : ControllerBase
 {
+
+    [HttpGet("[action]")]
+    public async Task<IActionResult> GetAllUsers()
+    {
+        return Ok();
+    }
+
+    [HttpGet("[action]/{id}")]
+    public async Task<IActionResult> GetUsrById(int id)
+    {
+        return Ok();
+    }
+
+    [HttpPost("[action]")]
+    public async Task<IActionResult> AddNewUser()
+    {
+        return Ok();
+    }
+
+    [HttpPut("[action]/{id}")]
+    public async Task<IActionResult> UpdateUser()
+    {
+        return Ok();
+    }
+
+    [HttpDelete("[action]/{id}")]
+    public async Task<IActionResult> DeleteUser(int id)
+    {
+        return Ok();
+    }
 }
